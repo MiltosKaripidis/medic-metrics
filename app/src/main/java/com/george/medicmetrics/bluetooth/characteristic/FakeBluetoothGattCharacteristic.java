b8e1b8214d33b7b4fee5571a534c201dbdd77db3
Miltos <miltos.karipidis@arx.gr>
2018-01-23 15:40:22 +0200

    Add fake alternatives

---
+++ app/src/main/java/com/george/medicmetrics/bluetooth/characteristic/FakeBluetoothGattCharacteristic.java
@@ -0,0 +1,61 @@
+package com.george.medicmetrics.bluetooth.characteristic;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.george.medicmetrics.bluetooth.descriptor.Descriptor;
+
+import java.util.List;
+import java.util.UUID;
+
+public class FakeBluetoothGattCharacteristic implements GattCharacteristic {
+
+    private String mUuid;
+    private List<Integer> mIntegerList;
+    private int mCurrent;
+    private List<Descriptor> mDescriptorList;
+
+    public FakeBluetoothGattCharacteristic(String uuid, List<Integer> integerList, List<Descriptor> descriptorList) {
+        mUuid = uuid;
+        mIntegerList = integerList;
+        mDescriptorList = descriptorList;
+    }
+
+    @NonNull
+    @Override
+    public UUID getUuid() {
+        return UUID.fromString(mUuid);
+    }
+
+    @Override
+    public int getProperties() {
+        return BluetoothGattCharacteristic.PROPERTY_NOTIFY;
+    }
+
+    @Nullable
+    @Override
+    public Integer getIntValue(int formatType, int offset) {
+        if (mCurrent == mIntegerList.size()) {
+            mCurrent = 0;
+        }
+        return mIntegerList.get(mCurrent++);
+    }
+
+    @Nullable
+    @Override
+    public byte[] getValue() {
+        return new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    }
+
+    @Nullable
+    @Override
+    public Descriptor getDescriptor(@NonNull UUID uuid) {
+        for (Descriptor descriptor : mDescriptorList) {
+            if (descriptor.getUuid().equals(uuid)) {
+                return descriptor;
+            }
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/george/medicmetrics/bluetooth/descriptor/FakeBluetoothDescriptor.java
@@ -0,0 +1,25 @@
+package com.george.medicmetrics.bluetooth.descriptor;
+
+import android.support.annotation.NonNull;
+
+import java.util.UUID;
+
+public class FakeBluetoothDescriptor implements Descriptor {
+
+    private UUID mUUID;
+
+    public FakeBluetoothDescriptor(UUID uuid) {
+        mUUID = uuid;
+    }
+
+    @NonNull
+    @Override
+    public UUID getUuid() {
+        return mUUID;
+    }
+
+    @Override
+    public void setValue(@NonNull byte[] value) {
+        // TODO: Implement
+    }
+}
